dd38bc601e8a7da1abc76c8b4bde48b2da8fcf02
nameishari <hariharakumar88@gmail.com>
2020-12-05 20:40:20 +0100

    Implement dynamic load of bundles (for each component)

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
 import {ThemeProvider} from './contexts/theme'
 import Nav from './components/Nav'
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
-import Results from './components/Results'
+import Loading from './components/Loading'
+
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
 
 // A component is concerned about
 // Component
@@ -36,13 +38,15 @@ class App extends React.Component {
                     <div className={this.state.theme}>
                         <div className='container'>
                             <Nav />
-                            <Switch>
-                                <Route exact path='/' component={Popular}/>
-                                <Route exact path='/battle' component={Battle}/>
-                                <Route path='/battle/results' component={Results} />
-                                {/* <Redirect from='/old' to='/new'/> */}
-                                <Route render={() => (<h1>404</h1>)}/>
-                            </Switch>
+                            <React.Suspense fallback={<Loading />} >
+                                <Switch>
+                                    <Route exact path='/' component={Popular}/>
+                                    <Route exact path='/battle' component={Battle}/>
+                                    <Route path='/battle/results' component={Results} />
+                                    {/* <Redirect from='/old' to='/new'/> */}
+                                    <Route render={() => (<h1>404</h1>)}/>
+                                </Switch>
+                            </React.Suspense>
                         </div>
                     </div>
                 </ThemeProvider>
