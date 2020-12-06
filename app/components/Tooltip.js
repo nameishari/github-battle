930a88fa6fb8f11683b3e356edd3c0b32719767c
nameishari <hariharakumar88@gmail.com>
2020-12-06 23:17:19 +0100

    Use custom hook instead of render-prop-pattern for hovering

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,47 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 //import withHover from './withHover'
-import Hover from './Hover'
+//import Hover from './Hover'
+import userHover from '../hooks/useHover'
 
 const styles = {
-    container: {
-      position: 'relative',
-      display: 'flex'
-    },
-    tooltip: {
-      boxSizing: 'border-box',
-      position: 'absolute',
-      width: '160px',
-      bottom: '100%',
-      left: '50%',
-      marginLeft: '-80px',
-      borderRadius: '3px',
-      backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
-      padding: '7px',
-      marginBottom: '5px',
-      color: '#fff',
-      textAlign: 'center',
-      fontSize: '14px',
-    }
+  container: {
+    position: 'relative',
+    display: 'flex'
+  },
+  tooltip: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    width: '160px',
+    bottom: '100%',
+    left: '50%',
+    marginLeft: '-80px',
+    borderRadius: '3px',
+    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
+    padding: '7px',
+    marginBottom: '5px',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: '14px',
   }
+}
 
-  export default function Tooltip({text, children}) { 
-    return (
-        <Hover>
-            {(hovering) => (
-                <div style={styles.container}>
-                {hovering === true && <div style={styles.tooltip}>{text}</div>}
-                {children}
-                </div>
-            )}
-        </Hover>
-    )
-  }
+export default function Tooltip({ text, children }) {
+  const [hovering, attrs] = userHover()
 
-  Tooltip.propTypes = {
-    text: PropTypes.string.isRequired, 
-  }
-  
+  return (
+    <div style={styles.container} {...attrs}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  )
+  // return (
+  //   <Hover>
+  //     {(hovering) => (
+  //       <div style={styles.container}>
+  //         {hovering === true && <div style={styles.tooltip}>{text}</div>}
+  //         {children}
+  //       </div>
+  //     )}
+  //   </Hover>
+  // )
+}
+
+Tooltip.propTypes = {
+  text: PropTypes.string.isRequired,
+}
+
 // function Tooltip({text, children, hovering}) {
 //     return (
 //         <div style={styles.container}>
